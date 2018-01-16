dfb2d8649d94200d734d2add5b9e029a2f7bb2ed
Paul Jacks <paul.jacks@scopevisio.com>
2018-01-16 13:49:34 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 
+
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule
