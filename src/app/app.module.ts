f641a771379d280c435bf49b213392d82d46f5a3
Paul Jacks <paul.jacks@scopevisio.com>
2018-01-29 17:31:37 +0100

    splash screen yay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { SplashscreenComponent } from './splashscreen/splashscreen.component';
 
+
+const appRoutes: Routes = [
+  { path: '**', component: AppComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
+    SplashscreenComponent
   ],
   imports: [
     BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      //{ enableTracing: true } // Logs all internal evens - should just be for testing. 
+    ),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/splashscreen/splashscreen.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-splashscreen',
+  templateUrl: './splashscreen.component.html',
+  styleUrls: ['./splashscreen.component.css']
+})
+export class SplashscreenComponent implements OnInit {
+  splashScreen; 
+
+  constructor() { }
+
+  ngOnInit() {
+    let splashScreen;
+    let imgSplashScreen;
+
+    removeSplashScreen();
+
+    function removeSplashScreen(){
+
+      splashScreen = document.getElementById("app-splash-screen");
+      imgSplashScreen = document.getElementById("img-splash-screen");
+
+      if(splashScreen != null){
+        imgSplashScreen.className += " fade-in-out";
+      }
+
+      window.setTimeout(e => {
+        splashScreen.style.display = "none";
+      }, 2000);
+    }
+    
+  }
+
+
+  
+
+}
