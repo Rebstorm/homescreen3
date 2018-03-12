4a06c763f76c4e30f7bbf746bd853f4107b4f79b
Paul Jacks <paul.jacks@scopevisio.com>
2018-03-12 10:24:05 +0100

    font awesome is font awesome

--- src/app/const/availabeApps.ts
+++ src/app/const/availabeApps.ts
@@ -3,19 +3,11 @@ import { WeatherComponent } from '../weather/weather.component';
 import { Directive } from '@angular/core'
 
 export const APPSAVAILABLE : App[] = [
-    { component : WeatherComponent,
+    { 
+      component : WeatherComponent,
       name : "weather",
       url: "/weather",
       description: "hello world",
-      imgurl: "../assets/images/park.svg";
+      imgurl: "../assets/images/park.svg"
     },
-    { component : WeatherComponent,
-      name : "hue Lights" 
-    },
-    { component : WeatherComponent,
-      name : "weather" 
-    },
-    { component : WeatherComponent,
-      name : "weather" 
-    }
 ];
